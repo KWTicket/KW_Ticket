f4eda63db93dcda61a7a335963b0b545766f4d27
qwert232323 <qwert232324@naver.com>
2019-11-13 22:57:01 +0900

    동적배열 할당 문제 해결 및 예약/예약확인 기능 추가
    동적배열 할당 문제 해결 및 예약/예약확인 기능 추가

--- Airplane.h
+++ Airplane.h
@@ -8,18 +8,12 @@ private:
 	int row;
 	int col;
 public:
-	Passenger** sit;
 	Airplane(){}
 	Airplane(string apName, string comName, int row, int col) {
 		this->apName = apName;
 		this->comName = comName;
 		this->row = row;
 		this->col = col;
-		
-		sit = new Passenger*[row];			// 2차원
-		for (int i = 0; i < row; i++) {		// 동적배열
-			sit[i] = new Passenger[col];	// 생성문
-		}
 
 	}
 	string getAp() {
@@ -52,6 +46,7 @@ private:
 	string depTime;
 	string arrTime;	
 public:
+	Passenger** sit;
 	Flight(){}
 	Flight(Airplane ap, string dep, string arr, string day, string dt, string at) {
 		this->ap = ap;
@@ -60,6 +55,11 @@ public:
 		this->day = day;
 		this->depTime = dt;
 		this->arrTime = at;
+
+		sit = new Passenger*[getAir().getRow()];			// 2차원
+		for (int i = 0; i < getAir().getRow(); i++) {		// 동적배열
+			sit[i] = new Passenger[getAir().getCol()];	// 생성문
+		}
 	}
 
 	Airplane getAir() {
@@ -92,11 +92,50 @@ public:
 	void viewSit(){
 		for (int i = 0; i < ap.getRow(); i++) {
 			for (int j = 0; j < ap.getCol(); j++) {
-				if (ap.sit[i][j].getPass() == 0)
+				if (sit[i][j].getPass() == 0)
 					cout << "[□]";
 				else cout << "[■]";
 			}
 			cout << endl;
-		}	
+		}
+	}
+
+	void reservation(Passenger passenger) {
+
+		int row = 0, col = 0;
+
+		cout << "좌석을 선택하세요" << endl;
+		viewSit();
+		cout << "행 : ";
+		cin >> row;
+		cout << "열 : ";
+		cin >> col;
+
+		if (sit[row - 1][col - 1].getPass() != 0)
+			cout << "이미 선택된 좌석입니다." << endl;
+		else {
+			for (int i = 0; i < ap.getRow(); i++) {
+				for (int j = 0; j < ap.getCol(); j++) {
+					if (sit[i][j].getPass() == passenger.getPass()) {
+						cout << "이미 이 비행기에 예약 되어 있습니다." << endl;
+						cout << "다시 예약 하시려면 기존 예약을 취소해주세요." << endl;
+						return;
+					}
+				}
+			}
+			sit[row - 1][col - 1] = passenger;
+			cout << "예약이 완료되었습니다." << endl;
+			viewSit();
+		}		
+	}
+	void checkReserve(Passenger passenger) {
+		cout << passenger.getName() << "님의 예약 정보입니다." << endl;
+		for (int i = 0; i < ap.getRow(); i++) {
+			for (int j = 0; j < ap.getCol(); j++) {
+				if (sit[i][j].getPass() == passenger.getPass()) {
+					test(); // 저희가 출력을 어떻게 할지에 따라서 달라질 예정입니다.
+				}
+			}
+		}
 	}
 };
