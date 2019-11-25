c188824c62cd3ee5fe2c68e4b2fbb3bfd7821212
YEERAMM <50645977+YEERAMM@users.noreply.github.com>
2019-11-25 18:44:35 +0900

    Update login_Advanced.cpp

--- login_Advanced.cpp
+++ login_Advanced.cpp
@@ -1,12 +1,22 @@
 #pragma once
 #include "User.h"
+#include <windows.h>
 
 Guest login(vector<Guest> DB) {
-
+	system("cls");
 	string a, b;
+	cout << "" << endl;
 
 	while (1) {
-		cout << "Input your ID : ";
+		cout << "[ Login ] ============================================" << endl;
+		cout << "∥                                                  ∥" << endl;
+		cout << "∥    ★ Please enter the correct information ★    ∥" << endl;
+		cout << "∥                                                  ∥" << endl;
+		cout << "∥ ★ If you want to go previous stage, press q. ★ ∥" << endl;
+		cout << "∥                                                  ∥" << endl;
+		cout << "=====================================================" << endl;
+		cout << "" << endl;
+		cout << "   ID : ";
 		cin >> a;
 		b = "";
 
@@ -14,20 +24,30 @@ Guest login(vector<Guest> DB) {
 
 			if (DB[i].getID() == a) {
 				while (1) {
-					cout << "Input your Password : ";
+					cout << "   Password : ";
 					cin >> b;
 
 					if (DB[i].getPW() == b) {
-						cout << "Hello," << DB[i].getName() << "!" << endl;
+						cout << "" << endl;
+						cout << "===============================================" << endl;
+						cout << "∥             Hello, " << DB[i].getName() << " > 0<!            ∥" << endl;
+						cout << "∥♡  Have a nice trip with KW-TICKET :-)  ♡∥" << endl;
+						cout << "===============================================" << endl;
+						cout << "" << endl;
+						Sleep(2000);
 						return DB[i];
 					}
 
 					else if (b == "q") {
+						cout << "" << endl;
+						system("cls");
 						break;
 					}
 
-					else {
-						cout << "*** Please enter your password correctly. ***" << endl;
+					else { //패스워드 정확하게
+						cout << "" << endl;
+						cout << "   !!!! The Password is not correct !!!!\n ";
+						cout << "   Please input the correct Password :-(" << endl;
 						continue;
 					}
 				}
@@ -39,17 +59,21 @@ Guest login(vector<Guest> DB) {
 				string c = "Null";
 				string d = "";
 				string e = "";
-
-				Guest Null(a,b,c,d,e);
+				Guest Null(a, b, c, d, e);
+				system("cls");
 				return Null;
 			}
 			if (b == "q")
 				break;
 		}
-		if (b == "q")
+		if (b == "q") {
+			cout << "" << endl;
 			continue;
+		}
 
-		cout << "*** Please enter your ID correctly. ***" << endl;
+		cout << "" << endl;
+		cout << "   !!!! The ID is not valid !!!!\n ";
+		cout << "   Please input a valid ID :-(" << endl;
 		cout << endl;
 	}
 }
